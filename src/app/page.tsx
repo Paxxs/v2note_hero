3fc6cdb3eb99caa6bdc0b37115f8ec367f4ff140
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-05-17 20:07:37 +0800

    feat: Initial implementation of software introduction display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+"use client";
+import { DescriptTionGrid } from "@/components/descript-grid";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Spotlight } from "@/components/ui/Spotlight";
-import Image from "next/image";
+import DotPattern from "@/components/ui/dot-pattern";
+import { LampContainer } from "@/components/ui/lamp";
+import TextReveal from "@/components/ui/text-reveal";
+import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import { useLayoutEffect } from "react";
 
 export default function Home() {
+  // const { setTheme } = useTheme();
+  // useLayoutEffect(() => {
+  //   setTheme("dark");
+  // });
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex min-h-dvh flex-col">
+      <TailwindIndicator />
       <section>
-        <div className="h-[40rem] w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="h-[20rem] md:h-[40rem] w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] overflow-hidden">
           <Spotlight
             className="-top-40 left-0 md:left-60 md:-top-20"
             fill="white"
           />
-          <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+          <div className=" p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
             <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
               V2Note <br /> The Way To Note.
             </h1>
@@ -19,6 +33,45 @@ export default function Home() {
             </p>
           </div>
         </div>
+      </section>
+      <section className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg bg-white dark:bg-black">
+        <TextReveal text="Experience the future of note-taking with v2Note: where dual editing meets seamless collaboration across devices." />
+      </section>
+      <section className="p-10 bg-black">
+        <DescriptTionGrid />
+      </section>
+      <section
+        id="open-source"
+        className="relative container py-8 md:py-12 lg:py-24 bg-background mt-8"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
+          <h2 className="font-extrabold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Proudly Open Source
+          </h2>
+          <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+            v2Note is open source and powered by open source software. <br />{" "}
+            The code is available on{" "}
+            <Link
+              href={""}
+              target="_blank"
+              rel="noreferrer"
+              className="underline underline-offset-4"
+            >
+              GitHub
+            </Link>
+            .{" "}
+          </p>
+        </div>
+        <DotPattern
+          width={20}
+          height={20}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+          )}
+        />
       </section>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,23 +5,78 @@ import colors from "tailwindcss/colors";
 // see tailwind.d.ts
 import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      animation: {
-        spotlight: "spotlight 2s ease .75s 1 forwards",
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
         spotlight: {
           "0%": {
             opacity: "0",
@@ -32,10 +87,26 @@ const config: Config = {
             transform: "translate(-50%,-40%) scale(1)",
           },
         },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
       },
     },
   },
   plugins: [
+    require("tailwindcss-animate"),
     addVariablesForColors,
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
@@ -60,8 +131,7 @@ const config: Config = {
       );
     },
   ],
-};
-export default config;
+} satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
@@ -73,3 +143,5 @@ function addVariablesForColors({ addBase, theme }: any) {
     ":root": newVars,
   });
 }
+
+export default config;
